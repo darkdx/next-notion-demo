243cddf650c49c4ce7c3acf22bdc2f7b57714d93
wysdx <wangyongsong@dxchain.com>
2023-02-24 13:54:20 +0800

    feat: add env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NOTION_ACCESS_TOKEN: 'secret_Dw1fM7oBtPQpObpTf151H6hNkL5kvsIBirN9fT8ou5v',
+    NOTION_DATABASE_USER_ID: '53da76eee8db4cc19e8c7020c828730a',
+  }
 }
 
 module.exports = nextConfig
